59ee553b7a5233aab9c72aa5912d9b7ac58edbd2
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-10-13 17:48:26 -0400

    Add react dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  output: 'hybrid',
+  integrations: [tailwind(), icon(), react()],
 });
