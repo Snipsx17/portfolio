0b1339dc31d657059ae5990f2dc8ebb0b8f566fe
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-12-29 21:50:02 -0500

    Feat: Add Overlay

---
+++ src/components/layout/nav/Overlay.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@components/lib/utils';
+
+export const Overlay = ({
+  show,
+  onClickHandler,
+}: {
+  show: boolean;
+  onClickHandler: () => void;
+}) => {
+  return (
+    <div
+      id="overlay"
+      className={cn(
+        'inset-0 bg-black/50 transition-opacity duration-300 lg:hidden',
+        show ? 'opacity-0 hidden' : 'opacity-100 fixed z-40'
+      )}
+      onClick={onClickHandler}
+    ></div>
+  );
+};
